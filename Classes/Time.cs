45e2e181371e0c7ba2987d2aaaebd4ddb3b18daf
Douglas de Menezes <dev@douglasdemenezes.com.br>
2020-01-19 21:37:38 -0300

    Berlin Clock implementation with passing tests

---
+++ Classes/Time.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerlinClock.Classes
+{
+    /// <summary>
+    /// Custom Time parser
+    /// </summary>
+    public class Time
+    {
+        private int hours;
+        public int Hours => hours;
+
+        private int minutes;
+        public int Minutes => minutes;
+
+        private int seconds;
+        public int Seconds => seconds;
+
+        public static Time Parse(string timeString)
+        {
+            string[] parts = timeString.Split(':');
+            if (parts.Length < 3) throw new ArgumentException("Invalid time provided. It should be in 'hh:mm:ss' format");
+
+            Time time = new Time();
+
+            if (!int.TryParse(parts[0], out time.hours) || time.hours < 0 || time.hours > 24) throw new ArgumentException("Invalid time provided. Hours must be between 00 and 24");
+            if (!int.TryParse(parts[1], out time.minutes) || time.minutes < 0 || time.minutes > 59) throw new ArgumentException("Invalid time provided. Minutes must be between 00 and 59");
+            if (!int.TryParse(parts[2], out time.seconds) || time.seconds < 0 || time.seconds > 59) throw new ArgumentException("Invalid time provided. Seconds must be between 00 and 59");
+
+            return time;
+        }
+    }
+}
--- Classes/TimeConverter.cs
+++ Classes/TimeConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BerlinClock.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +8,69 @@ namespace BerlinClock
 {
     public class TimeConverter : ITimeConverter
     {
+        /// <summary>
+        /// Lights off pattern
+        /// </summary>
+        private string[] _off =
+        {
+            "O",
+            "OOOO",
+            "OOOO",
+            "OOOOOOOOOOO",
+            "OOOO"
+        };
+
+        /// <summary>
+        /// Lights on pattern
+        /// </summary>
+        private string[] _on = 
+        {
+            "Y",
+            "RRRR",
+            "RRRR",
+            "YYRYYRYYRYY",
+            "YYYY"
+        };
+
         public string convertTime(string aTime)
         {
-            throw new NotImplementedException();
+            // Try parsing to a DateTime and use it to validate the input
+            Time time = Time.Parse(aTime);
+
+            // This will store the ammout of lights on per row
+            int[] rows = new int[5];
+
+            // The first row is the seconds, if it is even, then it is on
+            rows[0] = time.Seconds % 2 == 0 ? 1 : 0;
+
+            // For the hours, we need to divide exactly by 5 to know how many red should be on the 1st row
+            // And the rest of the division is the red on the 2nd row
+            rows[1] = time.Hours / 5;
+            rows[2] = time.Hours % 5;
+
+            // For the minutes, we need the same division as the hours
+            rows[3] = time.Minutes / 5;
+            rows[4] = time.Minutes % 5;
+
+            // The final string
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < 5; i++)
+            {
+                sb.AppendLine(getRow(i, rows[i]));
+            }
+            sb.Length = sb.Length - 2;
+
+            // Returns the final string
+            return sb.ToString();
         }
+
+        /// <summary>
+        /// Helper to get the row of the clock.
+        /// It combines the "on" lights with the "off" lights
+        /// </summary>
+        /// <param name="row">The number of the row (starts on 0)</param>
+        /// <param name="amount">The amout of lights on in the row</param>
+        /// <returns></returns>
+        private string getRow(int row, int amount) => string.Concat(_on[row].Substring(0, amount),  _off[row].Substring(amount));
     }
 }
